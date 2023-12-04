a7815c1e5e2b3207f8d06ef79445f97b4c6617d7
lombc <lomibaoc@uleth.ca>
2023-12-03 17:49:28 -0700

    Edits

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+
+
+/*
+// Change Theme Function
+$(document).ready(function () {
+    var theme = 'light';
+    $('#theme-button').click(function () {
+        if (theme == 'light') {
+            $('body').removeClass('light-theme');
+            $('body').addClass('dark-theme');
+            theme = 'dark';
+        } else {
+            $('body').removeClass('dark-theme');
+            $('body').addClass('light-theme');
+            theme = 'light';
+        }
+    });
+});
+*/
